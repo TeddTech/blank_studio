52cd460151b75d820adf8186b5f51b1e6547609e
Ted <ted@Teds-MacBook-Pro.local>
2019-07-04 18:30:26 -0700

    make it better

---
+++ __init__.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask
+from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_frozen import Freezer
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_pyfile('settings.py')
+    app.secret_key = 'development key'
+
+
+    # freezer = Freezer(app)
+
+
+
+    from .views import main
+    app.register_blueprint(main)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+
+    return(app)
