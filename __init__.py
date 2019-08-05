4e032e593cd5a5c0b9d3d9e429741ec5a8694182
Ted <ted@Teds-MacBook-Pro.local>
2019-08-04 17:29:54 -0700

    fdeployed 001

--- __init__.py
+++ __init__.py
@@ -14,14 +14,14 @@ app.secret_key = 'development key'
 # freezer = Freezer(app)
 
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('TeddTech/blank_studio')
-            origin = repo.remotes.origin
-origin.pull()
-return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('TeddTech/blank_studio')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 
